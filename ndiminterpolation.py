5296128b282e738a04e1d098ac29de1ac3095411
Robert Nikutta <robert.nikutta@gmail.com>
2016-06-28 16:47:20 -0400

    Replaced ndiminterpolation.py with fully vectorized version from hypercat development. Needs testing with existing codes, e.g. williitfit_mpi, since the API to __call__() has slightly changed.

--- ndiminterpolation.py
+++ ndiminterpolation.py
@@ -1,9 +1,10 @@
 __author__ = "Robert Nikutta <robert.nikutta@gmail.com>"
-__version__ = '20150416'
+__version__ = '20160626'
 
 import numpy as N
 import warnings
 from scipy import interpolate, ndimage
+import itertools
 
 # Convert RuntimeWarnings, e.g. division by zero in some array elements, to Exceptions
 warnings.simplefilter('error', RuntimeWarning)
@@ -37,13 +38,15 @@ class NdimInterpolation:
             array must be as if constructed via looping over all axes,
             i.e.
 
-              counter = 0
-              for j0 in theta[0]:
-                  for j1 in theta[1]:
-                     for j2 in theta[2]:
-                         ...
-                         hypercube[j0,j1,j2,...] = onedarray[counter]
-                         counter += 1
+            .. code:: python
+
+                counter = 0
+                for j0 in theta[0]:
+                    for j1 in theta[1]:
+                        for j2 in theta[2]:
+                            ...
+                            hypercube[j0,j1,j2,...] = onedarray[counter]
+                            counter += 1
 
         theta : list
             List of 1-d arrays, each holding in ascending order the
@@ -60,17 +63,17 @@ class NdimInterpolation:
 
         order : int
             Order of interpolation spline to be used. order=1
-            (default) is multi-linear interpolation, order=3 is
+            (default) is multi-linear interpolation, ``order=3`` is
             cubic-spline (quite a bit slower, and not necessarily
-            better, especially for complicated n-dim functions. order=1
-            is recommended.
+            better, especially for complicated n-dim
+            functions. ``order=1`` is recommended.
 
         mode : str
-            'log' is default, and will take log10(data) first, which
+            ``log`` is default, and will take log10(data) first, which
             severely improves the interpolation accuracy if the data
             span many orders of magnitude. This is of course only
-            applicable if all entries in 'data' are greater than
-            0. Any string other that 'log' will keep 'data' as-is.
+            applicable if all entries in `data` are greater than
+            0. Any string other that ``log`` will keep `data` as-is.
 
         Returns
         -------
@@ -80,11 +83,10 @@ class NdimInterpolation:
         -------
         General way to use ndiminterpolation
 
-          ipython --pylab
-          In[0]: import ndiminterpolation as nd
-          In[1]: ip, datacube, theta, mywave = nd.example()
+        .. code:: python
 
-        See also example.py in the same repository.
+            # to be written
+
         """
         
         self.theta = theta   # list of lists of parameter values, unique, in correct order
@@ -122,58 +124,106 @@ class NdimInterpolation:
             print "Done."
 
 
-    def get_coords(self,vector,pivots=None):
-        """Take real-world vector of parameter values, return image coordinates.
-
+    def get_coords(self,vec):
+        
+        """Construct a full 2D matrix of coordinates in pixel-space from a
+           vector of coordinates in real space.
+        
         Parameters
         ----------
+        vec : tuple
 
-        vector : 1-d array
-            Values of every parameter at which the interpolation
-            should be performed. This is on the input parameter
-            hypercube.
+            Tuple of lenght len(self.theta), with each element either
+            a single value of theta_i (of the i-th parameter), or
+            itself a tuple (of arbitrary length). If an element of vec
+            is itself a tuple (of length m), then m*(n-1) combinations
+            of all parameters will be added to the resulting 2D matrix
+            of coordinates.
 
-        pivots : 1-d array
-            Pivot points on the output, on which to deliver the
-            interpolation results.
+        Returns
+        -------
+        coorinates_pix : 2D array
+            2D array of coordinates in pixel space, on which then the
+            multi-dim interpolation can be performed.
+
+            Overall, A = N.prod([len(vec_i) for vec_i in vec])
+            coordinate sets, for B = len(self.theta) parameters, will
+            be generated, i.e. the returned coordinate matrix has
+            coordinates.shape = (A,B).
+
+        shape_ : tuple
+           The shape tuple to reshape coords_pix with to obtain a
+           properly shaped interpolated array.
+
+        Example
+        -------
+        .. code:: python
+
+            self.parameters
+                array(['a', 'b', 'c'])
+
+            [t.size for t in self.theta]
+                (3,5,2)
+
+            self.theta
+                [array(1.,2.,3.,), array(10.,15,18,24,26), array(100.,126)]
+
+            # vector of real-space coordinate to interpolate self.data_hypercube on
+            vec = (1.5,18.,110.)
+        
+            # compute pixel-space vector matrix, and shape of resulting array
+            coords_pix, shape_ = self.get_coords(vec)
 
 
-        Examples
-        --------
+        Old example, rework it:
 
-        vector = [0., 100.4, 2.4]
-        pivots = [1.,3.]   # can be e.g. microns, or the running number of a photometric band, etc.
+        .. code:: python
 
-        Then, get_coords(vector,pivots) returns the image coordinates at
-        parameter 1/2/3 = [0., 100.4, 2.4] and at pivot points = [1.,3.]
+            vec = (0,1,2,3,(0,1,2),(3,4,5),6)
+            vectup = [e if isinstance(e,tuple) else (e,) for e in vec]
+               [(0,), (1,), (2,), (3,), (0, 1, 2), (3, 4, 5), (6,)]
+            coords = N.array([e for e in itertools.product(*vectup)])  # in pixel space
+               array([[0, 1, 2, 3, 0, 3, 6],
+                      [0, 1, 2, 3, 0, 4, 6],
+                      [0, 1, 2, 3, 0, 5, 6],
+                      [0, 1, 2, 3, 1, 3, 6],
+                      [0, 1, 2, 3, 1, 4, 6],
+                      [0, 1, 2, 3, 1, 5, 6],
+                      [0, 1, 2, 3, 2, 3, 6],
+                      [0, 1, 2, 3, 2, 4, 6],
+                      [0, 1, 2, 3, 2, 5, 6]])
 
         """
+
+#        print "vec = ", vec
+
+        vectup = [e if isinstance(e,tuple) else (e,) for e in vec]
+#        print "vectup = ", vectup
+        shape_ = [len(e) for e in vectup]
+
+        coords_real = N.array([e for e in itertools.product(*vectup)])  # use ip(.) here
+#        print "coords_pix, coords_pix.shape = ", coords_pix, coords_pix.shape
         
-        len_vector = len(vector)
+        columns = coords_real.T.tolist()
+#        print "columns, len(columns) = ", columns, len(columns)
+        
+        coords_pix = N.array([ self.ips[j](columns[j]) for j in xrange(len(columns)) ])
+#        print "coords_pix.shape = ", coords_pix.shape
+#        print "self.data_hypercube.shape = ", self.data_hypercube.shape
+        
+        return coords_pix, shape_
 
-        if pivots == None:
-            pivots = self.theta[-1]   # pivots are theta without the wave/bands
 
-        coords = N.zeros((len_vector+1,pivots.size))  # 'x-prime'
-
-        for i,ip in enumerate(self.ips):
-            if i < len_vector:
-                # unfortunately we have to repeat most of the coordinate array,
-                # because we want the interpolated output on multiple pivot points
-                coords[i,:] = N.repeat( ip(vector[i]), pivots.size )
-            else: 
-                coords[i,:] = ip(pivots)
-
-        return coords
-            
-
-    def __call__(self,vector,pivots):
+    def __call__(self,vector):
         """Interpolate in N dimensions, using mapping to image coordinates."""
 
         if self.order == 1:
-            aux = ndimage.map_coordinates(self.data_hypercube,self.get_coords(vector,pivots=pivots),order=1)
-        elif self.order == 3:
-            aux = ndimage.map_coordinates(self.coeffs,self.get_coords(vector,pivots=pivots),order=3,prefilter=False)
+            coords, shape_ = self.get_coords(vector)
+            aux = ndimage.map_coordinates(self.data_hypercube,coords,order=1)
+            aux = aux.reshape(shape_)
+# temporarily disabled order==3, b/c not yet tested
+#        elif self.order == 3:
+#            aux = ndimage.map_coordinates(self.coeffs,self.get_coords(vector,pivots=pivots),order=3,prefilter=False)
 
         if self.mode == 'log':
             aux = 10.**aux

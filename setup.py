f15edf8efddb804ad2c37b89bdbf5834e8ef2b5e
Robert Nikutta <nikutta@noao.edu>
2018-07-27 20:10:38 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'ndiminterpolation',
-    version = '1.0.0',
+    version = '1.0.1',
     url = 'https://github.com/rnikutta/ndiminterpolation.git',
     author = 'Robert Nikutta',
     author_email = 'robert.nikutta@gmail.com',
